e214273909d9fe052e3b36efbb43ec1a1800e7dd
Anton Larionov <55048058+TonyCooT@users.noreply.github.com>
2021-11-02 18:56:05 +0300

    Добавил реализацию, исходные и обработанные данные

---
+++ src/opca/opca.cpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include "opca.h"
+
+Opca::Opca() {}
+Opca::~Opca() {}
+
+void Opca::opca(const cv::Mat& src, cv::Mat& dst)
+{
+    src.copyTo(dst);
+
+    bool isEnd = false;
+
+    while (!isEnd)
+    {
+        isEnd = true;
+
+        cv::Mat matrix(dst.size(), dst.type(), cv::Scalar(1));
+
+        for (int y = 1; y < (src.rows - 1); y++)
+        {
+            for (int x = 1; x < (src.cols - 1); x++)
+            {
+                if ((dst.at<uint8_t>(y, x) / UINT8_MAX) == 1)
+                {
+                    if (isFirstCondition(dst, y, x) && isSecondCondition(dst, y, x) && isThirdCondition(dst, y, x))
+                    {
+                        matrix.at<uint8_t>(y, x) = 0;
+                        isEnd = false;
+                    }
+                }
+            }
+        }
+
+        cv::multiply(dst, matrix, dst);
+    }
+
+    for (int y = 1; y < (src.rows - 1); y++)
+    {
+        for (int x = 1; x < (src.cols - 1); x++)
+        {
+            if ((dst.at<uint8_t>(y, x) / UINT8_MAX) == 1)
+            {
+                if (isLastCondition(dst, y, x))
+                    dst.at<uint8_t>(y, x) = 0;
+            }
+        }
+    }
+
+    return;
+}
+
+bool Opca::isFirstCondition(const cv::Mat& src, int y, int x)
+{
+    cv::Point2i points[8] = { cv::Point2i(x, y - 1),
+                              cv::Point2i(x + 1, y - 1),
+                              cv::Point2i(x + 1, y),
+                              cv::Point2i(x + 1, y + 1),
+                              cv::Point2i(x, y + 1),
+                              cv::Point2i(x - 1, y + 1),
+                              cv::Point2i(x - 1, y),
+                              cv::Point2i(x - 1, y - 1) };
+
+    int sum = 0;
+
+    for (int i = 0; i < 8; i++)
+        sum += src.at<uint8_t>(points[i]) / UINT8_MAX;
+
+    return (sum >= 2 && sum <= 6);
+}
+
+bool Opca::isSecondCondition(const cv::Mat& src, int y, int x)
+{
+    cv::Point2i points[8] = { cv::Point2i(x, y - 1),
+                              cv::Point2i(x + 1, y - 1),
+                              cv::Point2i(x + 1, y),
+                              cv::Point2i(x + 1, y + 1),
+                              cv::Point2i(x, y + 1),
+                              cv::Point2i(x - 1, y + 1),
+                              cv::Point2i(x - 1, y),
+                              cv::Point2i(x - 1, y - 1) };
+
+    int sum = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        int index = ((i + 2) - 2 + 1) % 8 + 2;
+        sum += abs(src.at<uint8_t>(points[i]) / UINT8_MAX - src.at<uint8_t>(points[index - 2]) / UINT8_MAX);
+    }
+
+    return (sum == 2);
+}
+
+bool Opca::isThirdCondition(const cv::Mat& src, int y, int x)
+{
+    cv::Point2i points[6] = { cv::Point2i(x, y - 1),
+                              cv::Point2i(x + 1, y),
+                              cv::Point2i(x, y + 1),
+                              cv::Point2i(x - 1, y),
+                              cv::Point2i(x + 2, y),
+                              cv::Point2i(x, y + 2) };
+
+    bool parts[6] = { 0 };
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (i == 1 || i == 2)
+            parts[i] = (src.at<uint8_t>(points[i]) / UINT8_MAX) == 1;
+        else
+            parts[i] = (src.at<uint8_t>(points[i]) / UINT8_MAX) == 0;
+    }
+
+    return !((parts[1] && parts[3] && parts[4]) || (parts[2] && parts[0] && parts[5]));
+}
+
+bool Opca::isLastCondition(const cv::Mat& src, int y, int x)
+{
+    cv::Point2i points[8] = { cv::Point2i(x, y - 1),
+                              cv::Point2i(x + 1, y - 1),
+                              cv::Point2i(x + 1, y),
+                              cv::Point2i(x + 1, y + 1),
+                              cv::Point2i(x, y + 1),
+                              cv::Point2i(x - 1, y + 1),
+                              cv::Point2i(x - 1, y),
+                              cv::Point2i(x - 1, y - 1) };
+
+    int rightCount = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        bool parts[3] = { 0 };
+
+        int indexOne = 2 * i + 1;
+        parts[0] = (src.at<uint8_t>(points[indexOne]) / UINT8_MAX) == 0;
+
+        int indexTwo = ((2 * (i + 1) + 1) - 2 + 3) % 8 + 2;
+        parts[1] = (src.at<uint8_t>(points[indexTwo - 2]) / UINT8_MAX) == 1;
+
+        int indexThree = ((2 * (i + 1) + 1) - 2 + 5) % 8 + 2;
+        parts[2] = (src.at<uint8_t>(points[indexThree - 2]) / UINT8_MAX) == 1;
+
+        if (parts[0] && parts[1] && parts[2])
+            rightCount++;
+    }
+
+    return (rightCount == 4);
+}
